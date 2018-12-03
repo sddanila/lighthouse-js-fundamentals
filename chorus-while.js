79f2fa243434e1a1a340d01f16711eb989499829
sddanila <sddanila@yahoo.co.uk>
2018-12-03 22:20:04 +0000

    Adding loop exercises - for and while

---
+++ chorus-while.js
@@ -0,0 +1,22 @@
+// VERY REPETITIVE - 10 TIMES
+// var chorus = "Let's Dance!";
+// var repeat = 0
+
+// while (repeat < 10){
+//   console.log(chorus);
+//   repeat++;
+// }
+
+// console.log("Until the sun comes up!");
+
+SOMEWHAT REPETITIVE - 5 TIMES / CHANGE KEY
+var chorus = "Let's dance!";
+var repeat = 0;
+while (repeat < 10) {
+  if (repeat === 5) {
+    console.log("*change key*");
+  }
+  console.log(chorus);
+  repeat++;
+}
+console.log("Until the sun comes up!");
